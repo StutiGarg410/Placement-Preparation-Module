fefa306192a92107518de43c80127c2ce765b4a9
Stuti Garg <86840177+StutiGarg410@users.noreply.github.com>
2023-12-04 00:16:31 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (81.85%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fibonacci(int n, int[] dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        dp[n] = fibonacci(n-1, dp)+fibonacci(n-2, dp);
+        return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        return fibonacci(n, dp);
+    }
+}
